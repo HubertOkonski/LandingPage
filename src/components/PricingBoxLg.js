ca3375cb0d84ec67bb79a99b4b1d66939d166e5b
Hubert Okonski <hubertokonski1@gmail.com>
2020-08-01 15:29:01 +0200

    before grid

--- src/components/PricingBoxLg.js
+++ src/components/PricingBoxLg.js
@@ -11,58 +11,63 @@ function PricingBoxLg() {
         </span>
         <Phone />
       </div>
-      <div className="pricing-box-header part-two-pricing part-two-boxlg">
+      <div className="pricing-box-header part-two-pricing part-two-boxlg"></div>
+      <div className="flexbox-container">
+        <div className="list-container">
+          <ul>
+            <li>Lorem ipsum</li>
+            <li>dolor sit amet, consectetur</li>
+            <li>adipiscing elit, sed do eiusmod</li>
+            <li>tempor incididunt</li>
+            <li>ut labore et dolore magna aliqua</li>
+            <li>Lorem ipsum</li>
+            <li>Ipsum dolor sit amet</li>
+            <li>ut labore et dolore magna aliqua</li>
+          </ul>
+          <footer className="register-try-box">
+            <div className="buttons-container">
+              <p>Zarejestruj się i wypróbuj!</p>
+              <button className="rg-button">Rejestracja</button>
+              <button className="lg-button">Logowanie</button>
+            </div>
+          </footer>
+        </div>
         <div className="basic sm-box">
           <p>Basic</p>
           <p>
             wersja <br /> darmowa
           </p>
+          <div className="checkmark-container">
+            <Checkmark />
+            <Checkmark />
+            <Checkmark />
+            <Checkmark />
+            <span className="times"> &#215;</span>
+            <span className="times"> &#215;</span>
+            <span className="times"> &#215;</span>
+          </div>
         </div>
         <div className="premium sm-box">
           <p>Premium</p>
-        </div>
-      </div>
-      <div className="list-container">
-        <ul>
-          <li>
-            Lorem ipsum <Checkmark />
-          </li>
-          <li>
-            dolor sit amet, consectetur
+          <select name="hall" id="hall" value="0">
+            <option>90 dni</option>
+            <option>60 dni</option>
+            <option>30 dni</option>
+          </select>
+          <p>
+            79,99zł <span className="sale-span">- 30% taniej</span>{" "}
+          </p>
+          <div className="checkmark-container">
             <Checkmark />
-          </li>
-          <li>
-            adipiscing elit, sed do eiusmod
             <Checkmark />
-          </li>
-          <li>
-            tempor incididunt
             <Checkmark />
-          </li>
-          <li>
-            ut labore et dolore magna aliqua
             <Checkmark />
-          </li>
-          <li>
-            Lorem ipsum
             <Checkmark />
-          </li>
-          <li>
-            Ipsum dolor sit amet
             <Checkmark />
-          </li>
-          <li>
-            ut labore et dolore magna aliqua
             <Checkmark />
-          </li>
-        </ul>
-        <footer className="register-try-box">
-          <div className="buttons-container">
-            <p>Zarejestruj się i wypróbuj!</p>
-            <button className="rg-button">Rejestracja</button>
-            <button className="lg-button">Logowanie</button>
+            <Checkmark />
           </div>
-        </footer>
+        </div>
       </div>
     </div>
   );
